2e63fb63527830cebcae52462febd8ac16adc27f
Oleg Kremliakov <olegdnepr@gmail.com>
2021-04-12 18:52:34 +0300

    First Commit

--- app/src/main/java/com/example/PizzaRecyclerview/SecondActivity.kt
+++ app/src/main/java/com/example/PizzaRecyclerview/SecondActivity.kt
@@ -1,12 +1,37 @@
 package com.example.PizzaRecyclerview
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.personrecyclerview.R.layout
+import kotlinx.android.synthetic.main.activity_second.*
 
 class SecondActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(layout.activity_second)
+//        var intent= Intent(this,SecondActivity::class.java)
+  //      val text:String = intent.getStringExtra("detail").toString()
+
+        val intent = getIntent();
+        val text = intent.getStringExtra("name")
+        val pricePizza = intent.getIntExtra("price",0)
+        val detailPizza = intent.getStringExtra("detail")
+        val imageURL = intent.getIntExtra("imageURL",0)
+
+
+        Toast.makeText(this, text, Toast.LENGTH_LONG).show()
+        name.text = text
+        price.text = pricePizza.toString()
+        detail.text = detailPizza
+        imageView.setImageResource(imageURL)
+
+
+
+
     }
 }
--- app/src/main/java/com/example/PizzaRecyclerview/data/PizzaListAdapter.kt
+++ app/src/main/java/com/example/PizzaRecyclerview/data/PizzaListAdapter.kt
@@ -1,21 +1,24 @@
 package com.example.PizzaRecyclerview.data
 
 import android.content.Context
-import android.media.Image
+import android.content.Intent
+import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
+import androidx.core.content.ContextCompat.startActivity
 import androidx.recyclerview.widget.RecyclerView
+import com.example.PizzaRecyclerview.SecondActivity
 import com.example.PizzaRecyclerview.data.PizzaListAdapter.ViewHolder
-import com.example.personrecyclerview.R
 import com.example.PizzaRecyclerview.model.Pizza
+import com.example.personrecyclerview.R
 
 class PizzaListAdapter(
-    private val list: List<Pizza>,
-    private val context: Context
+        private val list: List<Pizza>,
+        private val context: Context
 ): RecyclerView.Adapter<ViewHolder>() {
 
     inner class ViewHolder(itemView: View, context: Context): RecyclerView.ViewHolder(itemView) {
@@ -27,10 +30,19 @@ class PizzaListAdapter(
             var pizzaImage: ImageView = itemView.findViewById(R.id.picView)
             name.text = pizza.name
             price.text = pizza.price.toString()
-           pizzaImage.setImageResource(pizza.imageUrl)
+            pizzaImage.setImageResource(pizza.imageUrl)
 
             itemView.setOnClickListener(){
-                Toast.makeText(context, name.text, Toast.LENGTH_LONG).show()
+
+                val intent = Intent(context, SecondActivity::class.java)
+                intent.putExtra("name",pizza.name)
+                intent.putExtra("price",pizza.price)
+                intent.putExtra("detail", pizza.detail)
+                intent.putExtra("imageURL", pizza.imageUrl)
+
+                val bundle = Bundle()
+                startActivity(context, intent,bundle)
+
 
             }
         }
